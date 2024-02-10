aca5192770e5f15e61efe7f0b0a08cb9a8b96eac
Gorosss <90179487+Gorosss@users.noreply.github.com>
2024-02-10 01:43:42 +0100

    Foooter added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import '../css/Footer.css'
+
+export function Footer() {
+    return (
+        <footer className="footer">
+            <div>
+                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400"
+                >© 2024
+                    Jon Gorostegui Todos los derechos reservados.
+                </span>
+                <ul
+                    className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0"
+                >
+                    <li>
+                        <a href="#sobre_mi" className="hover:underline me-4 md:me-6">Sobre mi</a>
+                    </li>
+                    <li>
+                        <a href="mailto:jongorostegui@gmail.com" className="hover:underline">Contacto</a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
